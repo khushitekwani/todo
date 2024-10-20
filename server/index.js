0080bf7962e060e09421e7430d74b4b02f20a4c5
khushitekwani <97432551+khushitekwani@users.noreply.github.com>
2024-10-20 19:30:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,12 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
-app.use(cors(
-    {
-        origin: ['https://to-do-application-frontend-liard.vercel.app']
-    }
-    
-));
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
